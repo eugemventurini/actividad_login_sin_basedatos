ba3970dec27f9fed1d85b99011a6d0ba22235ba7
Eugenia Moreno <eugenia412009@gmail.com>
2022-08-04 16:59:40 -0300

    Form creado

---
+++ BugTrucker/FormularioLogin.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace BugTrucker
+{
+    partial class FormularioLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnUser = new System.Windows.Forms.Label();
+            this.lblnPsw = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPsw = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblnUser
+            // 
+            this.lblnUser.AutoSize = true;
+            this.lblnUser.Location = new System.Drawing.Point(54, 57);
+            this.lblnUser.Name = "lblnUser";
+            this.lblnUser.Size = new System.Drawing.Size(46, 15);
+            this.lblnUser.TabIndex = 0;
+            this.lblnUser.Text = "usuario";
+            // 
+            // lblnPsw
+            // 
+            this.lblnPsw.AutoSize = true;
+            this.lblnPsw.Location = new System.Drawing.Point(54, 91);
+            this.lblnPsw.Name = "lblnPsw";
+            this.lblnPsw.Size = new System.Drawing.Size(67, 15);
+            this.lblnPsw.TabIndex = 1;
+            this.lblnPsw.Text = "Contraseña";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(132, 54);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(266, 23);
+            this.txtUser.TabIndex = 2;
+            // 
+            // txtPsw
+            // 
+            this.txtPsw.Location = new System.Drawing.Point(132, 88);
+            this.txtPsw.Name = "txtPsw";
+            this.txtPsw.Size = new System.Drawing.Size(266, 23);
+            this.txtPsw.TabIndex = 3;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(285, 156);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(366, 156);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // FormularioLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 197);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtPsw);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblnPsw);
+            this.Controls.Add(this.lblnUser);
+            this.Name = "FormularioLogin";
+            this.Text = "FormularioLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblnUser;
+        private Label lblnPsw;
+        private TextBox txtUser;
+        private TextBox txtPsw;
+        private Button btnIngresar;
+        private Button btnSalir;
+    }
+}
---
+++ BugTrucker/FormularioLogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugTrucker
+{
+    public partial class FormularioLogin : Form
+    {
+        public FormularioLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if ((txtUser.Text == ""))
+            {
+                MessageBox.Show("Se debe ingresar un usuario!");
+                return;
+            }
+
+            if ((txtPsw.Text == ""))
+            {
+                MessageBox.Show("Se debe ingresar una contraseña!");
+                return;
+            }
+            MessageBox.Show("Usted ha ingresado al sistema!");
+            this.Close();
+        }
+    }
+}
